cf7e569b8ac742eaaae3781f784a2a0ea6938222
Moe <rezaalipour.mohammad@gmail.com>
2023-07-15 16:39:44 +0200

    Blacken setup.py and add requirements and modify description

--- setup.py
+++ setup.py
@@ -4,23 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='annotest',
-    version='0.1',
+    name="annotest",
+    version="0.1",
     author="Mohammad Rezaalipour",
     author_email="rezaalipour.mohammad@gmail.com",
-    description="A tool to automatically generating bug-finding inputs for NN program testing.",
+    description="A tool to automatically generating bug-finding inputs for neural network program testing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/atom-sw/annotest",
-    packages=setuptools.find_packages(exclude=('tests')),
-    install_requires=[
-        "astor~=0.8.1",
-        "autoflake~=1.4",
-        "hypothesis~=6.4",
-    ],
-    python_requires='>=3.6',
-    entry_points={
-        'console_scripts': [
-            'annotest = annotest.__main__:main'
-        ]
-    })
+    packages=setuptools.find_packages(exclude=("tests")),
+    install_requires=["astor~=0.8.1", "autoflake~=1.4", "hypothesis~=6.4", "black"],
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["annotest = annotest.__main__:main"]},
+)
